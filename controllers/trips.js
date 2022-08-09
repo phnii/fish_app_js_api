6bd9de77abd1aac9b63ec66a8b63131490333f48
phnii <sudiryaya@gmail.com>
2022-08-09 22:32:56 +0900

    エラーハンドリング

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,9 +1,11 @@
+const ErrorResponse = require("../utils/ErrorResponse");
+const asyncHandler = require("../middleware/async");
 const Trip = require("../models/Trip");
 
 // @desc    Get all trips
 // @route   GET /trips
 // @access  Public
-exports.getTrips = async (req, res, next) => {
+exports.getTrips = asyncHandler(async (req, res, next) => {
   const trips = await Trip.find();
 
   if (!trips) {
@@ -14,13 +16,13 @@ exports.getTrips = async (req, res, next) => {
     success: true,
     count: trips.length,
     data: trips
+  });
 });
-}
 
 // @desc    Get single trip
 // @route   GET /trips/:id
 // @access  Public
-exports.getTrip = async (req, res, next) => {
+exports.getTrip = asyncHandler(async (req, res, next) => {
   const trip = await Trip.findById(req.params.id);
 
   if (!trip) {
@@ -31,24 +33,24 @@ exports.getTrip = async (req, res, next) => {
     success: true,
     data: trip
   });
-}
+});
 
 // @desc    Create trip
 // @route   POST /trips
 // @access  Private
-exports.createTrip = async (req, res, next) => {
+exports.createTrip = asyncHandler(async (req, res, next) => {
   const trip = await Trip.create(req.body);
 
   res.status(201).json({
     success: true,
     data: trip
   });
-}
+});
 
 // @desc    Update trip
 // @route   PUT /trips/:id
 // @access  Private
-exports.updateTrip = async (req, res, next) => {
+exports.updateTrip = asyncHandler(async (req, res, next) => {
   const trip = await Trip.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
@@ -62,12 +64,12 @@ exports.updateTrip = async (req, res, next) => {
     success: true,
     data: trip
   });
-}
+});
 
 // @desc    Delete trip
 // @route   DELETE /trips/:id
 // @access  Private
-exports.deleteTrip = async (req, res, next) => {
+exports.deleteTrip = asyncHandler(async (req, res, next) => {
   const trip = await Trip.findByIdAndRemove(req.params.id);
 
   if (!trip) {
@@ -77,5 +79,5 @@ exports.deleteTrip = async (req, res, next) => {
     success: true,
     data: {}
   });
-}
+});
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
-
+const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
 // Load env vars
@@ -27,6 +27,8 @@ if (process.env.NODE_ENV === "development") {
 
 // Mount routers
 app.use("/trips", trips);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
