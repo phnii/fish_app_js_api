1c7d93fd857dd5e66929fe891714e5948e5cf30e
phnii <sudiryaya@gmail.com>
2022-08-09 19:41:53 +0900

    DB接続設定

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
-// Route files
-const trips = require("./routes/trips");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env"});
 
+// Connect to database
+connectDB();
+
+// Route files
+const trips = require("./routes/trips");
+
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 
 // Mount routers
 app.use("/trips", trips);
 
 const PORT = process.env.PORT || 5000;
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
 
 app.listen(
   PORT,
