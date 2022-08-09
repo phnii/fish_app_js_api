89b65bb10fc8523e860238d0ad7dc545b533c378
phnii <sudiryaya@gmail.com>
2022-08-09 21:03:53 +0900

    Tripモデル作成

---
+++ models/trip.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const tripSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "タイトルを入力してください"],
+    maxLength: [20, "タイトルは20字以下で入力してください"]
+  },
+  prefecture: {
+    type: Number,
+    min: 0,
+    max: 47
+  },
+  content: {
+    type: String,
+    required: [true, 内容を入力してください],
+    maxLength: [2000, "2000字以内で入力してください"]
+  },
+  // fishes: [{type:Schema.Types.ObjectId, ref: "Fish"}],
+  // user: {type: Schema.Types.ObjectId, ref: "User", required: true},
+  // comments: [{type: Schema.Types.ObjectId, ref: "Comment"}],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Trip", tripSchema);
